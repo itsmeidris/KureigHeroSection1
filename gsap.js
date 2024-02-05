e2da8a2df2e2a7425af9200dc0636756a73238a5
itsmeidris <idriissloulidi@gmail.com>
2024-02-06 00:36:53 +0100

    Final Version

--- gsap.js
+++ gsap.js
@@ -1,41 +1,59 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Initial animations
     gsap.from(".heroSection", {
-        opacity: 2,
-        duration: 1,
-        y: 50,
-        ease: "power2.out"
+      opacity: 2,
+      duration: 1,
+      y: 50,
+      ease: "power2.out"
     });
 
     gsap.from(".bigTitle", {
-        opacity: 0,
-        duration: 3,
-        y: 50,
-        delay: 0.5,
-        ease: "power2.out"
+      opacity: 0,
+      duration: 3,
+      y: 50,
+      delay: 0.5,
+      ease: "power2.out"
     });
 
     gsap.from(".description, .btnDetails", {
-        opacity: 0,
-        duration: 3,
-        y: 50,
-        delay: 1,
-        stagger: 0.2,
-        ease: "power2.out"
+      opacity: 0,
+      duration: 3,
+      y: 50,
+      delay: 1,
+      stagger: 0.2,
+      ease: "power2.out"
     });
 
     gsap.from(".sales", {
-        opacity: 0,
-        duration: 3,
-        y: 50,
-        delay: 1.5,
-        ease: "power2.out"
-    }); 
+      opacity: 0,
+      duration: 3,
+      y: 50,
+      delay: 1.5,
+      ease: "power2.out"
+    });
 
     gsap.from(".mainContainer", {
-        opacity: 0,
-        duration: 3,
-        y: 100,
-        delay: 1,
-        ease: "power2.out"
+      opacity: 0,
+      duration: 3,
+      y: 100,
+      delay: 1,
+      ease: "power2.out"
     });
-});
+
+    // Scroll animations
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('sec2, .newPro,.coffeePics,.section-container, #promotion, .testimonials, footer').forEach((section, index) => {
+      gsap.from(section, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+          trigger: section,
+          start: 'top center+=100',
+          end: 'bottom center+=100',
+          toggleActions: 'play none none reverse'
+        }
+      });
+    });
+  });
